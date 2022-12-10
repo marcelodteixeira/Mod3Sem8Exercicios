300e629bfd962177670b9edbe7742b039c156eec
marcelodteixeira <marcelo.lemaf@gmail.com>
2022-12-10 19:18:26 -0300

    Ex01Sem10Mod3

---
+++ Mod3Sem10Ex1/src/About.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Descricao extends React.Component {
+
+render (){
+
+    return(
+        <><h1>Marcelo</h1><p>
+            Aluno do curso FuturoDev
+        </p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+            <li>Java</li>
+            <li>SpringBoot</li>
+            <li>SQL</li>
+            <li>Postgres</li>
+            <li>JavaScrpit</li>
+            <li>ReactJS</li>
+        </ul>
+        </>
+
+    )
+
+}
+
+     
+}
+
+export default Descricao;
+
---
+++ Mod3Sem10Ex1/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Descricao from './About';
+
+
+
+class App extends React.Component{
+
+  render(){
+
+    return (
+    <Descricao>
+      
+    </Descricao>
+    )
+    
+  }
+
+}
+
+export default App;
